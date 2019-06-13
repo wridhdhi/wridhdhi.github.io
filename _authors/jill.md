8b456ca362ab5a88c149d970986c2f14aafd610f
Wridhdhi Karar <wskarar@gmail.com>
2019-06-13 11:03:57 +0530

    first commit

---
+++ _authors/jill.md
@@ -0,0 +1,8 @@
+---
+short_name: jill
+name: Jill Smith
+position: Chief Editor
+layout: author_layout
+---
+
+Jill is an avid fruit grower based in the south of France.
---
+++ install_ruby.sh
@@ -0,0 +1,4 @@
+echo '# Install Ruby Gems to ~/gems' >> ~/.bashrc
+echo 'export GEM_HOME="$HOME/gems"' >> ~/.bashrc
+echo 'export PATH="$HOME/gems/bin:$PATH"' >> ~/.bashrc
+source ~/.bashrc
